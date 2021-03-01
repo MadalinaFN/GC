04a31f6a2917ffbaa5e270ed883d8491292037fa
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-03-01 17:58:45 +0200

    Laborator1

--- Laborator 1/Program.cs
+++ Laborator 1/Program.cs
@@ -12,6 +12,92 @@ namespace Laborator_1
         {
             //EcuatieSiDistanta();
             //Coliniare();
+            //Concurenta();
+        }
+
+        private static void Concurenta()
+        {
+            double a1, b1, c1, a2, b2, c2, a3, b3, c3, rez;
+
+            Console.WriteLine("Introduceti a1, b1, c1");
+            a1 = double.Parse(Console.ReadLine());
+            b1 = double.Parse(Console.ReadLine());
+            c1 = double.Parse(Console.ReadLine());
+
+            if (b1 < 0 && c1 > 0)
+            {
+                Console.WriteLine($"d1:{a1}x {b1}y + {c1} = 0");
+            }
+            if (b1 > 0 && c1 < 0)
+            {
+                Console.WriteLine($"d1:{a1}x + {b1}y {c1} = 0");
+            }
+            if (b1 > 0 && c1 > 0)
+            {
+                Console.WriteLine($"d1:{a1}x + {b1}y + {c1} = 0");
+            }
+            if (b1 < 0 && c1 < 0)
+            {
+                Console.WriteLine($"d1:{a1}x {b1}y {c1} = 0");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Introduceti a2, b2, c2");
+            a2 = double.Parse(Console.ReadLine());
+            b2 = double.Parse(Console.ReadLine());
+            c2 = double.Parse(Console.ReadLine());
+
+            if (b2 < 0 && c2 > 0)
+            {
+                Console.WriteLine($"d2:{a2}x {b2}y + {c2} = 0");
+            }
+            if (b2 > 0 && c2 < 0)
+            {
+                Console.WriteLine($"d2:{a2}x + {b2}y {c2} = 0");
+            }
+            if (b2 > 0 && c2 > 0)
+            {
+                Console.WriteLine($"d2:{a2}x + {b2}y + {c2} = 0");
+            }
+            if (b2 < 0 && c2 < 0)
+            {
+                Console.WriteLine($"d2:{a2}x {b2}y {c2} = 0");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Introduceti a3, b3, c3");
+            a3 = double.Parse(Console.ReadLine());
+            b3 = double.Parse(Console.ReadLine());
+            c3 = double.Parse(Console.ReadLine());
+
+            if (b3 < 0 && c3 > 0)
+            {
+                Console.WriteLine($"d3:{a3}x {b3}y + {c3} = 0");
+            }
+            if (b3 > 0 && c3 < 0)
+            {
+                Console.WriteLine($"d3:{a3}x + {b3}y {c3} = 0");
+            }
+            if (b3 > 0 && c3 > 0)
+            {
+                Console.WriteLine($"d3:{a3}x + {b3}y + {c3} = 0");
+            }
+            if (b3 < 0 && c3 < 0)
+            {
+                Console.WriteLine($"d3:{a3}x {b3}y {c3} = 0");
+            }
+            Console.WriteLine();
+
+            rez = a1 * b2 * c3 + a2 * b3 * c1 + b1 * c2 * a3 - (c1 * b2 * a3) - (c2 * b3 * a1) - (a2 * b1 * c3);
+
+            if (rez == 0)
+            {
+                Console.WriteLine("Sunt concurente");
+            }
+            else
+            {
+                Console.WriteLine("Nu sunt concurente");
+            }
         }
 
         private static void Coliniare()
